dea46c5701e14b8a3d7f9ce7aa7bcab397b93e52
tostangs <toharaoa@asu.edu>
2018-01-15 21:08:37 -0700

    Close to working with minor bugs

--- src/Game.cpp
+++ src/Game.cpp
@@ -1,61 +1,89 @@
 //============================================================================
 // Name        : Hello.cpp
-// Author      : 
-// Version     :
+// Author      : Not me haha
+// Version     : idk honestly
 // Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
+// Description : Three dots for depression, the game for the excitement... ;)
 //============================================================================
 
 #include <iostream>
 #include <cstdlib>
 #include <windows.h>
+#include <stdlib.h>
+#include <cstdlib>
+#include <ctime>
 #include "conio.h"
 
 using namespace std;
-bool gameOver;
+bool beginGame, gameOver;
 const int width = 20, height = 20;
 int x, y, fruitX, fruitY, score;
+int tailX[100], tailY[100];
+int nTails;
 enum eDirection {STOP = 0, UP, DOWN, LEFT, RIGHT};
 eDirection dir;
 
-
 void Setup() {
+	// The beginning of the program...
+	beginGame = true;
 	gameOver = false;
 	dir = STOP;
 	x = width / 2;
 	y = height / 2;
-	fruitX = rand() % width;
-	fruitY = rand() % height;
+
+	// The creation of the location of the fruit...
+	srand(time(NULL));
+	fruitX = rand()%(19);
+	srand(time(NULL));
+	fruitY = rand()%(19);
 	score = 0;
 }
 
 void Draw() {
 
-//	system ("CLS");
-	clrscr();
-	for(int j = 0 ; j < height ; ++j) {
-		for(int i = 0; i < width ;++i) {
+	// This is the startup screen in the beginning of the game... :)
+	if (beginGame) {
+		cout << "Press any button to play :)" << endl;
+		system ("PAUSE");
+		beginGame = false;
+	}
 
-		   if(i == 0 || i == (width - 1) || j == 0 || j == (height - 1))
+	// This clears the screen... Though it's not very good lol I'm so lonely...
+	system ("CLS");
+
+	// This is the logic to draw the 20x20 grid aka the barriers...
+	for(int j = 0 ; j < height ; ++j) {
+
+		for(int i = 0; i < width ; ++i) {
+
+		   if(i == 0 || i == (width-1) || j == 0 || j == (height-1))
 			   cout << "# ";
 		   else if(i == fruitX && j == fruitY)
 			   cout << "$ ";
 		   else if(i == x && j == y)
 			   cout << "O ";
-		   else
-			   cout << "  ";
+		   else {
+			   bool print = false;
+			   for (int k = 0; k < nTails; ++k) {
 
-
+				   if(tailX[k] == i && tailY[k] == j) {
+					   cout << "o ";
+					   print = true;
+				   }
+			   }
+			   if(!print)
+				   cout << "  ";
+		   }
 		}
 		cout << endl;
 	}
 
-
 }
 
 void Input() {
-	if (_kbhit()) {
 
+	// Changes the enum variable 'eDirection' based on the input...
+	if (_kbhit()) {
 		switch (_getch()) {
 			case 'a':
 				dir = LEFT;
@@ -73,13 +101,36 @@ void Input() {
 				gameOver = true;
 				break;
 		}
-
 	}
-
 }
 
 void Logic() {
 
+	// This creates the logic for the tail of the Snake
+	int prevX = tailX[0];
+	int prevY = tailY[0];
+	int prev2X, prev2Y;
+	tailX[0] = x;
+	tailY[0] = y;
+	for (int i = 1; i < nTails; i++) {
+
+		if (tailX[i] == x && tailY[i] == y)
+			gameOver = true;
+
+		prev2X = tailX[i];
+		prev2Y = tailY[i];
+		tailX[i] = prevX;
+		tailY[i] = prevY;
+		prevX = prev2X;
+		prevY = prev2Y;
+
+	}
+
+	// This creates the barriers and the triggers the game to end if crossed...
+	if (x >= width-1 || x == 0 || y >= height-1 || y == 0)
+			gameOver = true;
+
+	// Logic behind the changing in direction.
 	switch (dir) {
 		case STOP:
 			break;
@@ -97,18 +148,24 @@ void Logic() {
 			break;
 	}
 
-	if (x > width-1 || x < 1 || y > height-1 || y < 1)
-		gameOver = true;
-
 	if(x == fruitX && y == fruitY) {
+
+		// Increases the score by 10, then generates another position for the fruit and adds a segment to the tail...
 		score += 10;
-		fruitX = rand() % width-1;
-		fruitY = rand() % height-1;
+		nTails++;
+		srand(time(NULL));
+		fruitX = rand()%(19);
+		srand(time(NULL));
+		fruitY = rand()%(19);
 
+		// This is to test whether the candy is at the right location
+		// cout << "new X " << fruitX << " & new Y" << fruitY << endl;
+		// system("PAUSE");
 	}
-
 }
 
+
+// Honestly this function might be totally useless...  ( I copied it from online to see if it's more efficient than system("CLS"), probably not though... )
 void ClearScreen (void) {
 	DWORD n;                         /* Number of characters written */
 	DWORD size;                      /* number of visible characters */
@@ -134,14 +191,30 @@ void ClearScreen (void) {
 
 int main() {
 
-	Setup();
-	while(!gameOver) {
-		Draw();
-		Input();
-		Logic();
-		ClearScreen();
-	}
-	system ("PAUSE");
+	bool restart = false;
+	string res;
+	do {
+		restart = false;
+		reset();
+		Setup();
+		while(!gameOver) {
+			Draw();
+			Sleep(10);
+			Input();
+			Logic();
+		}
+
+		system ("CLS");
+		cout << "Game Over...	You Scored: " << score << " Points" << ", the fruit is at " << fruitX << ", " << fruitY << endl << endl;
+		cout << "Press 'R' to play again." << endl;
+		cin >> res;
+		if(res == "r" || res == "R") {
+			restart = true;
+			gameOver = false;
+		}
+		system ("PAUSE");
+
+	} while (restart);
 
 	return 0;
 }
